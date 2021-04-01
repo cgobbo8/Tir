1c2eb998de32d5f5905f10107777994c3e4b477a
cgobbo8 <corentin.gobbo@hotmail.fr>
2021-04-01 17:13:13 +0200

    resolving font issue#2

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,12 @@ export default {
             { name: "viewport", content: "width=device-width, initial-scale=1" },
             { hid: "description", name: "description", content: "" }
         ],
-        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: "preconnect", href: "https://fonts.gstatic.com" },
+            { rel: "stylesheet", href: "//fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&display=swap" }
+        ]
     },
-
 
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [],
